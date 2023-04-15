5b09c773414635f31696456dc3f52fa451bacadd
Andrew Cate <acate5@gmail.com>
2023-04-15 12:41:23 -0700

    Created UnitOfMeasure class and created 1:1 relationship (unidirectional) from Ingredient to UnitOfMeasure

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,29 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description; // unidirectional so no relationship with Ingredient from this side
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return description;
+    }
+
+    public void setUom(String uom) {
+        this.description = uom;
+    }
+}
