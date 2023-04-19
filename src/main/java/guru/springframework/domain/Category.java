6132965715aacbd2029e25d94ef870fe34aefee9
Andrew Cate <acate5@gmail.com>
2023-04-18 17:33:43 -0700

    Adds Lombok and refactors several classes.

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,8 +1,12 @@
 package guru.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data   // Refactor > DeLombok to undo
+@EqualsAndHashCode(exclude = {"recipes"})   // exclude recipes property from @EqualsAndHashCode Lombok annotation to avoid stackoverflow error d/t javax.persistence @ManyToMany
 @Entity
 public class Category {
 
@@ -15,27 +19,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
