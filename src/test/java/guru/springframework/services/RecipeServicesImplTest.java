24427e6874615842730de7f8db95fdde04e48e71
Andrew Cate <acate5@gmail.com>
2023-04-18 18:05:21 -0700

    Adds several unit tests.

---
+++ src/test/java/guru/springframework/services/RecipeServicesImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServicesImplTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this); // creates mock recipeRepo
+
+        recipeService = new RecipeServicesImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();   // find all called once and only once
+    }
+}
